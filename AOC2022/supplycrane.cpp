2e4715dd307068854e55b59a50fa3f7e2b27ecf8
Siva kumar <tvsivakumar001@gmail.com>
2022-12-05 18:27:47 +0530

    fixed the parse input.

--- AOC2022/supplycrane.cpp
+++ AOC2022/supplycrane.cpp
@@ -4,11 +4,11 @@
 #include<string>
 #include<regex>
 
-// part 1
-std::string solve(std::map<int, std::stack<char>> container_stack, std::vector<std::vector<int>> moving_data)
+///////////// part 1
+std::string solve1(std::vector<std::stack<char>> container_stack, std::vector<std::vector<int>> moving_data)
 {
     for(auto itr: moving_data){
-        int times = itr[0], from=itr[1], to=itr[2];
+        int times = itr[0], from=itr[1]-1, to=itr[2]-1;
         while(times--){
             container_stack[to].push(container_stack[from].top());
             container_stack[from].pop();
@@ -16,17 +16,17 @@ std::string solve(std::map<int, std::stack<char>> container_stack, std::vector<s
     }
     std::string ans;
     for(auto it: container_stack){
-        ans += it.second.top();
+        ans += it.top();
     }
     return ans;
 }
 
 
-// part 2
-std::string solve(std::map<int, std::stack<char>> container_stack, std::vector<std::vector<int>> moving_data)
+///////////// part 2
+std::string solve2(std::vector<std::stack<char>> container_stack, std::vector<std::vector<int>> moving_data)
 {
     for(auto itr: moving_data){
-        int times = itr[0], from=itr[1], to=itr[2];
+        int times = itr[0], from=itr[1]-1, to=itr[2]-1;
         std::stack<char> tmp;
         while(times--){
             tmp.push(container_stack[from].top());
@@ -40,116 +40,61 @@ std::string solve(std::map<int, std::stack<char>> container_stack, std::vector<s
     }
     std::string ans;
     for(auto it: container_stack){
-        ans += it.second.top();
+        ans += it.top();
     }
     return ans;
 }
 
+
 int main()
 {
-    // i apologize for the bad input here.. :(
-    std::stack<char> stk1, stk2, stk3, stk4, stk5, stk6, stk7, stk8, stk9;
-
-    // stk1.push('Z');
-    // stk1.push('N');
-
-    // stk2.push('M');
-    // stk2.push('C');
-    // stk2.push('D');
-
-    // stk3.push('P');
-
-    stk1.push('T');
-    stk1.push('P');
-    stk1.push('Z');
-    stk1.push('C');
-    stk1.push('S');
-    stk1.push('L');
-    stk1.push('Q');
-    stk1.push('N');
-
-
-    stk2.push('L');
-    stk2.push('P');
-    stk2.push('T');
-    stk2.push('V');
-    stk2.push('H');
-    stk2.push('C');
-    stk2.push('G');
-
-    stk3.push('D');
-    stk3.push('C');
-    stk3.push('Z');
-    stk3.push('F');
-
-    stk4.push('G');
-    stk4.push('W');
-    stk4.push('T');
-    stk4.push('D');
-    stk4.push('L');
-    stk4.push('M');
-    stk4.push('V');
-    stk4.push('C');
-
-    stk5.push('P');
-    stk5.push('W');
-    stk5.push('C');
-
-    stk6.push('P');
-    stk6.push('F');
-    stk6.push('J');
-    stk6.push('D');
-    stk6.push('C');
-    stk6.push('T');
-    stk6.push('S');
-    stk6.push('Z');
-
-    stk7.push('V');
-    stk7.push('W');
-    stk7.push('G');
-    stk7.push('B');
-    stk7.push('D');
-
-    stk8.push('N');
-    stk8.push('J');
-    stk8.push('S');
-    stk8.push('Q');
-    stk8.push('H');
-    stk8.push('W');
-
-    stk9.push('R');
-    stk9.push('C');
-    stk9.push('Q');
-    stk9.push('F');
-    stk9.push('S');
-    stk9.push('L');
-    stk9.push('V');
-
-
-    std::map<int, std::stack<char>> container_stack;
-    container_stack.insert({1, stk1});
-    container_stack.insert({2, stk2});
-    container_stack.insert({3, stk3});
-    container_stack.insert({4, stk4});
-    container_stack.insert({5, stk5});
-    container_stack.insert({6, stk6});
-    container_stack.insert({7, stk7});
-    container_stack.insert({8, stk8});
-    container_stack.insert({9, stk9});
+    std::vector<std::stack<char>> container_stack;
+    std::vector<std::vector<int>> moving_data;
 
     std::ifstream Myfile("input.txt");
-    std::vector<std::vector<int>> moving_data;
+    std::vector<std::string> InputFileAsString;
+
+    // convert the file into an array of strings
     if(Myfile.is_open()){
-        std::regex ptrn("[ a-z]+(\\d+)[ a-z]+(\\d+)[ a-z]+(\\d+)");
-        std::smatch match;
-        int count=0,from=0,to=0;
-
-
         std::string line;
         while(std::getline(Myfile, line)){
-            std::regex_match(line, match, ptrn);
+            InputFileAsString.push_back(line);
+        }
+    }
+
+    // find the space b/w the stack lines and move part lines
+    int blankLine=0;
+    for(auto line: InputFileAsString){
+        if(line.size()==0){
+            break;
+        }
+        blankLine++;
+    }
+
+    // now add the above stack part into a real stack ds
+    std::string stk_line = InputFileAsString[blankLine-1];
+    for(int i=0;i<stk_line.size();i++){
+        if(stk_line[i]==' ')continue;
+        std::stack<char> tmp_stk;
+
+        for(int j=blankLine-2;j>=0;j--){
+            if(InputFileAsString[j][i]==' ' || InputFileAsString[j].size()<=i)break;
+            tmp_stk.push(InputFileAsString[j][i]);
+        }
+        container_stack.push_back(tmp_stk);
+    }
+
+    // simple regex to store 3 groups of numbers among letters
+    std::regex ptrn("move (\\d+)[ a-z]+(\\d+)[ a-z]+(\\d+)");
+    std::smatch match;
+    for(auto line: InputFileAsString){
+        int count=0,from=0,to=0;
+        if(std::regex_match(line, match, ptrn)){
             moving_data.push_back({stoi(match[1]), stoi(match[2]), stoi(match[3])});
         }
     }
-    std::cout << solve(container_stack, moving_data);
+
+    std::cout << solve1(container_stack, moving_data) << std::endl;
+    std::cout << solve2(container_stack, moving_data) << std::endl;
+
 }
