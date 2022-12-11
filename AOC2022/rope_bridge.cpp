17704287be7f908b39445e2888e3a9341543352b
Siva kumar <tvsivakumar001@gmail.com>
2022-12-11 16:03:09 +0530

    comments added

--- AOC2022/rope_bridge.cpp
+++ AOC2022/rope_bridge.cpp
@@ -1,59 +1,51 @@
-#include<bits/stdc++.h>
-#include<iostream>
-#include<fstream>
-#include<string>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <unordered_set>
+#include <bits/stdc++.h>
 
+#define ll long long
 using namespace std;
 
-int solve1(vector<pair<char, int>> fileData){
-    vector<pair<int,int>> tail_locations(9, {0,0});
-
+inline bool shouldMove(const pair<int, int> &fst, const pair<int, int> &snd) {
+    return abs(fst.first - snd.first) > 1 || abs(fst.second - snd.second) > 1;
 }
 
-pair<int, int> get_near_tail(pair<int, int> head, pair<int, int> tail){
-    if(abs(head.first-tail.first)>=2){
-        if(head.second<tail.second){
-            tail.second--;
-        }
-        else{
-            tail.second++;
-        }
-    }
-    else if(abs(head.second-tail.second)>=2){
-        if(head.first<tail.first){
-            tail.first--;
-        }
-        else{
-            tail.first++;
-        }
-    }
-    else if(abs(head.first-tail.first)>=2 && abs(head.second-tail.second)>=2){
-        if(head.first<tail.first){
-            tail.first--;
-        }
-        else{
-            tail.first++;
-        }
-        if(head.second<tail.second){
-            tail.second--;
-        }
-        else{
-            tail.second++;
-        }
-    }
-    return tail;
+constexpr inline int compare(const int &fst, const int &snd) {
+    return fst == snd ? 0 : (fst > snd ? 1 : -1);
 }
 
-int main(){
-    ifstream Myfile("inp.txt");
+size_t simulate(size_t rope_size) {
+    ifstream infile("inp.txt");
+    string line;
+    vector<pair<int, int>> rope(rope_size, make_pair(0, 0));
+    unordered_set<ll> visited;
 
-    vector<pair<char, int>> fileData;
-    if(Myfile.is_open()){
-        string line;
-        while(getline(Myfile, line)){
-            int tiles = stoi(line.substr(2, line.size()-2));
-            fileData.push_back(make_pair(line[0], tiles));
+    while (getline(infile, line)) {
+        char direction = line[0];
+        size_t steps = stoi(line.substr(2));
+        for (size_t j = 0; j < steps; ++j) {
+            switch(direction) {
+                case 'U': rope[0].second += 1; break;
+                case 'D': rope[0].second -= 1; break;
+                case 'L': rope[0].first -= 1; break;
+                default: rope[0].first += 1; break;
+            }
+
+            for (int i = 1; i < rope_size && shouldMove(rope[i - 1], rope[i]); ++i) {
+                rope[i].second += compare(rope[i - 1].second, rope[i].second);
+                rope[i].first += compare(rope[i - 1].first, rope[i].first);
+            }
+
+            visited.insert(((ll) rope.back().first) * 10000 + rope.back().second);
         }
     }
-    cout << solve1(fileData) << endl;
+
+    infile.close();
+    return visited.size();
+}
+
+int main() {
+    cout << simulate(2) << endl;
+    cout << simulate(10) << endl;
 }
